8266f287c6363833e54b3dbbe6f7dc000fbf4f44
NathanzaGaurangga <nathnzagaurangga@gmail.com>
2025-11-17 11:40:18 +0800

    checkponint, implement html on javascript, show and hide login form, tidy up the html

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-// Login menggunakan submit form
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.addEventListener("DOMContentLoaded", checkLoginStatus)
+
+function checkLoginStatus() {
+    const token = localStorage.getItem("access-token")
+    const loginStatus = document.getElementById("login-status")
+
+    loginStatus.innerHTML = ""
+
+    if (token) {
+        loginStatus.innerHTML = `
+            <div class="card">
+                <h2>You already Logged In</h2>
+                <p>Do you want to logout?</p>
+                <button id="logout-btn">Logout</button>
+            </div>
+        `
+
+        document.getElementById("logout-btn").addEventListener("click", logout)
+
+    } else {
+        loginStatus.innerHTML = `
+            <form id="login-form" method="post">
+                <div class="card">
+                    <h2>Login</h2>
+
+                    <input type="text" id="username" placeholder="Username" required>
+                    <input type="password" id="password" placeholder="Password" required>
+
+                    <button type="submit" id="login-btn" style="margin-top: 5px;">Login</button>
+
+                    <div id="login-message" class="message"></div>
+                </div>
+            </form>
+        `
+
+        document.getElementById("login-form").addEventListener("submit", login)
+    }
+}
+
+function login(event) {
     event.preventDefault()
 
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
-
     const msg = document.getElementById("login-message")
-
-    console.log(username, password)
 
     fetch("https://dummyjson.com/auth/login", {
         method: "POST",
@@ -20,44 +55,64 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         if (data.accessToken) {
-            localStorage.setItem("local-storage", data.accessToken)
+            localStorage.setItem("access-token", data.accessToken)
             msg.style.color = "green"
-            msg.textContent = "Login berhasil"
+            msg.textContent = "Login Successful"
+
+            setTimeout(checkLoginStatus, 500)
         } else {
             msg.style.color = "red"
-            msg.textContent = "Username atau password salah"
+            msg.textContent = "Wrong username or password"
         }
     })
-})
+}
 
-// Tombol tampilkan data
+
+function logout() {
+    localStorage.removeItem("access-token")
+    checkLoginStatus()
+}
+
+
 document.getElementById("show-data-btn").addEventListener("click", fetchUserData)
 
 function fetchUserData() {
-    const token = localStorage.getItem("local-storage")
+    const token = localStorage.getItem("access-token")
 
     const authMsg = document.getElementById("auth-message")
-    const userdata = document.getElementById("userdata")
+    const collectionBox = document.getElementById("collection-data")
 
     authMsg.textContent = ""
-    userdata.classList.add("hidden")
+    collectionBox.innerHTML = ""
+    collectionBox.classList.add("hidden")
 
     if (!token) {
-        authMsg.textContent = "Anda belum login"
+        authMsg.textContent = "Please login first to see user data."
         return
     }
 
-    fetch("https://dummyjson.com/auth/me", {
-        headers: { "Authorization": `Bearer ${token}` }
+    fetch("https://dummyjson.com/c/cd87-475c-493b-bd2d", {
+        headers: { Authorization: `Bearer ${token}` }
     })
     .then(res => res.json())
-    .then(data => {
-        document.getElementById("user-name").textContent = data.firstName + " " + data.lastName
-        document.getElementById("user-email").textContent = data.email
-        document.getElementById("user-username").textContent = data.username
+    .then(result => {
+        result.data.forEach(item => {
+            const div = document.createElement("div")
+            div.className = "card-item"
+            div.innerHTML = `
+                <p><b>ID:</b> ${item.id}</p>
+                <p><b>First Name:</b> ${item.first_name}</p>
+                <p><b>Last Name:</b> ${item.last_name}</p>
+                <hr>
+            `
+            collectionBox.appendChild(div)
+        })
 
-        userdata.classList.remove("hidden")
+        collectionBox.classList.remove("hidden")
+    })
+    .catch(err => {
+        authMsg.textContent = "Failed to fetch data."
+        console.error(err)
     })
 }
